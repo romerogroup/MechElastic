55cf313b0800f016366728c2f32f3123485af0e8
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-08-12 22:22:01 -0400

    Updated version info

--- mechelastic/version.py
+++ mechelastic/version.py
@@ -1,11 +1,11 @@
 
 # THIS FILE IS GENERATED FROM MECHELASTIC SETUP.PY.
 name = 'MechElastic'
-version = '1.1.12'
+version = '1.1.14'
 description = 'A Python library to calculate elastic properties of materials. '
 url = 'https://github.com/romerogroup/MechElastic'
 author = 'Sobhit Singh'
 email = 'smsingh@mail.wvu.edu'
 status = 'development'
 copyright = 'Copyright 2020'
-date = 'July 30th, 2020'
+date = 'Aug 6th, 2020'
