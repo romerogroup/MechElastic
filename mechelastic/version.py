e113e931ed5b7bb35ff03b3387bafaa9cac84b7d
Uthpala Herath <uthpala@Uthpalas-MacBook-Pro.local>
2021-01-09 23:04:30 -0500

    Updated docs

--- mechelastic/version.py
+++ mechelastic/version.py
@@ -1,11 +1,11 @@
 
 # THIS FILE IS GENERATED FROM MECHELASTIC SETUP.PY.
 name = 'MechElastic'
-version = '1.2.7'
+version = '1.2.8'
 description = 'A Python library to calculate elastic properties of materials. '
 url = 'https://github.com/romerogroup/MechElastic'
 author = 'Sobhit Singh, Logan Lang, Viviana Dovale-Farelo, Uthpala Herath, Pedram Tavadze, François-Xavier Coudert, Aldo H. Romero'
 email = 'smsingh@mail.wvu.edu'
 status = 'development'
 copyright = 'Copyright 2021'
-date = 'Jan 5th, 2021'
+date = 'Jan 9th, 2021'
