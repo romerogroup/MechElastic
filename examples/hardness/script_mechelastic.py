3839edb66306b55281fbe871a875419bd81c8955
vdovale29 <68760107+vdovale29@users.noreply.github.com>
2021-04-19 23:18:59 -0400

    Update script_mechelastic.py

--- examples/hardness/script_mechelastic.py
+++ examples/hardness/script_mechelastic.py
@@ -3,7 +3,4 @@
 import mechelastic
 mechelastic.calculate_elastic()
 
-# you can also get the information from the materials project using the following command
-# please modify the api_key to your personal api_key from https://materialsproject.org/dashboard
-
-mechelastic.from_mp(mp_id="mp-66", api_key="omguEiVsQNBOzaBT")
+# Use this script to run MechElastic from an OUTCAR VASP file
