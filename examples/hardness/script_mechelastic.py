7fc018e7a65c745a70ec6197b56653c8181343ac
vdovale29 <68760107+vdovale29@users.noreply.github.com>
2021-04-19 23:12:13 -0400

    Update script_mechelastic.py

--- examples/hardness/script_mechelastic.py
+++ examples/hardness/script_mechelastic.py
@@ -6,4 +6,4 @@ mechelastic.calculate_elastic()
 # you can also get the information from the materials project using the following command
 # please modify the api_key to your personal api_key from https://materialsproject.org/dashboard
 
-# mechelastic.from_mp(mp_id="mp-66", api_key="omguEiVsQNBOzaBT")
+mechelastic.from_mp(mp_id="mp-66", api_key="omguEiVsQNBOzaBT")
