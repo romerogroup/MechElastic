1fff1f786ac8821d8ac0e9a5c1401a6115c08d47
Uthpala Herath <ukh0001@mix.wvu.edu>
2021-04-21 11:42:46 -0400

    Updated examples for EOS

--- examples/EOS/enthalpy-curves/run_enthalpy.py
+++ examples/EOS/enthalpy-curves/run_enthalpy.py
@@ -4,6 +4,6 @@ from mechelastic import EOS
 
 eos_object = EOS()
 
-infiles = ["Rm-3m", "Pm-3m", "Im-3m", "I4_mcm", "C12-m1"]
+infiles = ["Rm-3m", "Pm-3m", "Im-3m", "I4_mcm", "C2-m"]
 natoms = [6, 1, 2, 9, 4]
 eos_object.plot_enthalpy_curves(infiles, natoms, au=True)
