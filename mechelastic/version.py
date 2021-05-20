dbb8d463b41fb7169988a5ac89bd1ae4f6337758
Uthpala Herath <ukh0001@mix.wvu.edu>
2021-05-20 00:18:11 -0400

    Removed test-mechelastic.py from repo

--- mechelastic/version.py
+++ mechelastic/version.py
@@ -1,11 +1,11 @@
 
 # THIS FILE IS GENERATED FROM MECHELASTIC SETUP.PY.
 name = 'MechElastic'
-version = '1.2.20'
+version = '1.2.21'
 description = 'A Python library to calculate elastic properties of materials. '
 url = 'https://github.com/romerogroup/MechElastic'
 author = 'Sobhit Singh, Logan Lang, Viviana Dovale-Farelo, Uthpala Herath, Pedram Tavadze, François-Xavier Coudert, Aldo H. Romero'
 email = 'smsingh@mail.wvu.edu'
 status = 'development'
 copyright = 'Copyright 2021'
-date = 'May 5th, 2021'
+date = 'May 20th, 2021'
--- test-mechelastic.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import mechelastic
-
-mechelastic.calculate_elastic(
-    "./examples/VASP/OUTCAR-Si_bulk",
-)
